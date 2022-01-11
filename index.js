622db8026f0f9cacf4aa5032968249aeea6dd4c7
emileypalmquist <emiley.palmquist@icloud.com>
2022-01-11 09:17:04 -0800

    added second condition to scuberGreetingForFeet

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function scuberGreetingForFeet(ride){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
   if (ride <= 400) {
     return "This one is on me!";
+  } else if (ride > 2000) {
+    return "I will gladly take your thirty bucks.";
   }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity() {
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
 }
